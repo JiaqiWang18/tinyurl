127be9e290ed5caecc4b3a0853d88469d936af03
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2022-01-29 00:27:19 -0500

    add validation logic

--- shortener/src/main/java/com/jwang/shortener/controller/UrlController.java
+++ shortener/src/main/java/com/jwang/shortener/controller/UrlController.java
@@ -2,12 +2,14 @@ package com.jwang.shortener.controller;
 
 import com.jwang.shortener.service.UrlService;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,7 +29,7 @@ public class UrlController {
         String originalUrl = urlService.retrieveOriginalUrl(hash);
         Map<String, Object> res = new HashMap<>();
         if(originalUrl == null){
-            res.put("error", "url not found");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "url not found");
         }
         res.put("data", originalUrl);
         return res;
@@ -35,14 +37,22 @@ public class UrlController {
 
     @PostMapping("/generate")
     public Map<String, Object> createShortUrl(@RequestBody Map<String, String> reqBody){
-        String originalUrl = reqBody.get("original");
-        String shortenUrl;
         Map<String, Object> res = new HashMap<>();
 
+        if(!reqBody.containsKey("original")){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "field original must be present!");
+        }
+        String originalUrl = reqBody.get("original");
+        String shortenUrl;
+
         if(reqBody.containsKey("hash")){
-            shortenUrl = urlService.customizeShortenUrl(originalUrl, reqBody.get("hash"));
+            String hash = reqBody.get("hash");
+            if(hash.isEmpty() || hash.length() > 10){
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "customized url size is invalid!");
+            }
+            shortenUrl = urlService.customizeShortenUrl(originalUrl, hash);
             if(shortenUrl==null){
-                res.put("error", "url in use");
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "url in use, try another one");
             }
         }else{
             shortenUrl = urlService.generateShortenUrl(originalUrl);
