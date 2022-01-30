737c51d1e9037d640bf9f6e2c7b0258714718735
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2022-01-29 20:27:56 -0500

    finish frontend v1

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import NavBar from "../components/NavBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
