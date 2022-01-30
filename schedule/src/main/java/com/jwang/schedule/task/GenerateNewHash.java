88dc7c63a76c132915156b562e819258d169f230
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2022-01-30 01:15:21 -0500

    add a schedule service

---
+++ schedule/src/main/java/com/jwang/schedule/task/GenerateNewHash.java
@@ -0,0 +1,21 @@
+package com.jwang.schedule.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class GenerateNewHash {
+    private static final Logger log = LoggerFactory.getLogger(GenerateNewHash.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        log.info("The time is now {}", dateFormat.format(new Date()));
+    }
+}
---
+++ schedule/src/test/java/com/jwang/schedule/ScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwang.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
