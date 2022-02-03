5c3443e8773be0d24a8d86fb3cfe47f4d321135b
Jiaqi (Jacky) Wang <jacky200218@gmail.com>
2022-02-03 15:22:09 -0500

    refine login logic with redux

--- frontend/api/index.ts
+++ frontend/api/index.ts
@@ -1,5 +1,5 @@
 const api =
   process.env.NODE_ENV === "development"
     ? "http://localhost:88"
-    : "https://tinyurl.jackywang.us/";
+    : "https://tinyurl.jackywang.us";
 export default api;
